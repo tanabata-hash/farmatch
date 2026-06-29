230b61d997307d56581462fd9c385516319f6e96
tanabata-hash <tanabata119@mac.com>
2026-06-29 15:30:46 +0900

    Update App.jsx

--- farmatch/src/App.jsx
+++ farmatch/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { supabase } from "./supabase";
 import { TermsPage, PrivacyPage, SpecifiedCommercialPage } from "./pages/Legal";
 import { AuthModal } from "./components/Auth";
